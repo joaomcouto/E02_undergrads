a03429d2446db1508e89956d5d83d3482c9ca35c
BrenoCPimenta <brenocpimenta@gmail.com>
2021-04-12 04:53:51 -0300

    Add run and env files

---
+++ news_scraper/env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['PROJECT_DIR'] = os.path.dirname(os.path.abspath(__file__))
+
+os.environ['CHROME'] = os.getenv('PROJECT_DIR') + '/../resources/drivers/chromedriver'
+os.environ['FIREFOX'] = os.getenv('PROJECT_DIR') + '/../resources/drivers/geckodriver'
--- news_scraper/run.py
+++ news_scraper/run.py
@@ -1,14 +1,33 @@
+from crawler.outline import Outline
+import nltk
 import sys
 
 
+def initialize():
+    """
+    Execute initial commands for enviroment.
+    """
+    # necessary for nlp in newspaper library:
+    nltk.download('punkt')
+
+
 def run(argv):
+    """
+    Execute and generate json file.
+    """
     interface = False
 
-    if argv[0] == '-i':
+    if "-init" in argv:
+        initialize()
+
+    # Verify interface use:
+    if '-I' in argv:
         interface = True
-        url = argv[1]
-    else:
-        url = argv[0]
+
+    url_news = argv[-1]
+    outline = Outline(interface)
+    data = outline.getNews(url_news)
+    print(data)
 
 
 if __name__ == "__main__":
