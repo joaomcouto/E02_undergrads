0e290edc739129d90092c71e3d520204a72fc2e9
BrenoCPimenta <brenocpimenta@gmail.com>
2021-04-13 16:55:40 -0300

    Solve Bug on Lupa 'Mostrar Mais'

--- 1/crawler_checagens.py
+++ 1/crawler_checagens.py
@@ -1,35 +1,38 @@
 import newspaper
 from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import WebDriverWait       
-from selenium.webdriver.common.by import By       
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 #sources = ['https://piaui.folha.uol.com.br/lupa','https://www.aosfatos.org/','https://www.boatos.org/','https://projetocomprova.com.br/','https://g1.globo.com/fato-ou-fake/','https://www.e-farsas.com/']
+
 
 def crawler_lupa():
     base_url = 'https://piaui.folha.uol.com.br/lupa'
     url_list = []
 
-    driver = webdriver.Firefox()
+    # driver = webdriver.Firefox()
+    driver = webdriver.Firefox(executable_path="./geckodriver")
     driver.get(base_url)
 
+    wait = WebDriverWait(driver, 15)
+
     i = 0
-    while i<10:
+    while i < 10:
         class_block = driver.find_elements_by_class_name("lista-noticias")
         for block in class_block:
             elements = block.find_elements_by_tag_name("a")
         for el in elements:
             url_list.append(el.get_attribute("href"))
-        
-        button = driver.find_element_by_xpath("//a[@class='btn-mais btnvermais']")
-        button.click()
         time.sleep(1)
-        new_page = driver.page_source()
-        driver.get(new_page)
+        button = driver.find_element_by_xpath("//a[@class='btn-mais btnvermais']")
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(1)
         i += 1
-
+    driver.close
     return set(url_list)
+
 
 def crawler_fato_fake():
     base_url = 'https://g1.globo.com/fato-ou-fake/'
@@ -39,12 +42,11 @@ def crawler_fato_fake():
     class_block = driver.find_elements_by_xpath("/html/body/div[2]/main/div[4]/div[2]/div/div/div/div/div/div/div")
     for block in class_block:
         elements = driver.find_elements_by_tag_name("a")
-    
+
     for el in elements:
         url_list.append(el.get_attribute("href"))
 
     return url_list
-
 
 
 lista = crawler_lupa()
