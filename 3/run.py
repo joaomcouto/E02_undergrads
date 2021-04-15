f4f550d9bae0281464d06b1f3390da8ce78c8243
joaomcouto <joaom.couto55@gmail.com>
2021-04-15 16:30:19 -0300

    G1 initial modularization, pending other sources plus utils module

--- 3/run.py
+++ 3/run.py
@@ -5,6 +5,14 @@
 # for article in cnn_paper.articles:
 #     print(article.url)
 
+import os
+import env
+
+
+
+
+from news_crawler.base_crawler import BaseCrawler
+
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -17,62 +25,85 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 
-PATH = "/home/joaomcouto/chromedriver"
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
-chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--verbose')
-chrome_options.add_experimental_option("prefs", {
-    "download.default_directory": "",
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing_for_trusted_sources_enabled": False,
-    "safebrowsing.enabled": False
-})
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-software-rasterizer')
+#driver = BaseCrawler()
 
-driver = webdriver.Chrome(PATH,options=chrome_options)
+# PATH = "/home/joaomcouto/chromedriver"
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--window-size=1920x1080")
+# chrome_options.add_argument("--disable-notifications")
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--verbose')
+# chrome_options.add_experimental_option("prefs", {
+#     "download.default_directory": "",
+#     "download.prompt_for_download": False,
+#     "download.directory_upgrade": True,
+#     "safebrowsing_for_trusted_sources_enabled": False,
+#     "safebrowsing.enabled": False
+# })
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-software-rasterizer')
+
+#driver = webdriver.Chrome(os.getenv('CHROME') )#,options=chrome_options)
 
 def printURLs(lista):
     for URL in lista:
         print(URL, "\n")
 
-def crawler_g1_pages(startPage, endPage):
-    delay = 10 # seconds
-    URLs = []
-    for page in range(startPage,endPage):
-        start = time.time()
-        driver.get("https://g1.globo.com/bemestar/coronavirus/index/feed/pagina-" + str(page) + ".ghtml")
-        try:
-            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "feed-placeholder")))
-            print ("Feed encontrado, varrendo pagina", page, "...")
-        except TimeoutException:
-            print ("Feed não localizado! Indicativo de varredura completa ou erro de conexão..\n")
-            printURLs(URLs)
-            print (" !!!! Coletamos",len(URLs), "URLs")
-            driver.close()
-            sys.exit(0)
 
-        
-        feedElement = driver.find_element_by_id("feed-placeholder")
+class g1_crawler(BaseCrawler):
+    def __init__(self, interface):
+        """
+        Create chosen base crawler driver 
+        """
+        # Choosing browser based on iterface parameter
+        if interface:
+            super(g1_crawler, self).__init__("firefox")
+        else:
+            super(g1_crawler, self).__init__()
 
-        materias = feedElement.find_elements_by_xpath('//div[@data-type ="materia"]')
+        # Parameters:
+        #self.wait_rate = 180
+        #self.outline_url = "https://outline.com/"
 
-        URLCount = 0
-        for materia in materias:
-            URLs.append(materia.find_element_by_tag_name('a').get_attribute('href'))
-            URLCount +=1 
-        print("\t", URLCount, "URLs encontradas em" , round(time.time() - start ,2) ,"segundos" )
+        # Set wait limit time for elements search
+        #self.wait = WebDriverWait(self.driver, self.wait_rate)
 
-        f=open('G1_1140_a_1150.txt','w')
-        for u in URLs:
-            f.write(u +'\n')
-        f.close()
+    def crawl_g1_pages(self,startPage, endPage):
+        delay = 10 # seconds
+        URLs = []
+        for page in range(startPage,endPage):
+            start = time.time()
+            self.driver.get("https://g1.globo.com/bemestar/coronavirus/index/feed/pagina-" + str(page) + ".ghtml")
+            try:
+                myElem = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.ID, "feed-placeholder")))
+                print ("Feed encontrado, varrendo pagina", page, "...")
+            except TimeoutException:
+                print ("Feed não localizado! Indicativo de varredura completa ou erro de conexão..\n")
+                printURLs(URLs)
+                print (" !!!! Coletamos",len(URLs), "URLs")
+                self.driver.close()
+                sys.exit(0)
 
-#crawler_g1_pages(1140,1150)
+            
+            feedElement = self.driver.find_element_by_id("feed-placeholder")
+
+            materias = feedElement.find_elements_by_xpath('//div[@data-type ="materia"]')
+
+            URLCount = 0
+            for materia in materias:
+                URLs.append(materia.find_element_by_tag_name('a').get_attribute('href'))
+                URLCount +=1 
+            print("\t", URLCount, "URLs encontradas em" , round(time.time() - start ,2) ,"segundos" )
+
+            f=open('G1_1140_a_1150.txt','w')
+            for u in URLs:
+                f.write(u +'\n')
+            f.close()
+
+g1 = g1_crawler(0)
+g1.crawl_g1_pages(1140,1150)
+
 
 def crawler_uol():
     delay = 10 # seconds
@@ -126,7 +157,7 @@ def crawler_bbc(startPage, endPage):
             print (" !!!! Coletamos",len(URLs), "URLs")
             driver.close()
             sys.exit(0)
-        time.sleep(1)
+        time.sleep(2)
         
         feedElement = driver.find_element_by_id("lx-stream")
 
@@ -138,16 +169,16 @@ def crawler_bbc(startPage, endPage):
             URLCount +=1 
         print("\t", URLCount, "URLs encontradas em" , round(time.time() - start ,2) ,"segundos" )
 
-        #f=open('G1_1140_a_1150.txt','w')
-        #for u in URLs:
-        #    f.write(u +'\n')
-        #f.close()
+        f=open('BCC_1_a_100.txt','w')
+        for u in URLs:
+            f.write(u +'\n')
+        f.close()
 
     printURLs(URLs)
     print (" !!!! Coletamos",len(URLs), "URLs")
     driver.close()
     sys.exit(0)
 
-crawler_bbc(98,101)
+#crawler_bbc(1,100)
 
 
