e78d1523cf1edaa8e53f26c6a3141777d7723553
BrenoCPimenta <brenocpimenta@gmail.com>
2021-04-19 12:59:14 -0300

    Add first version of custom crawler and other minor changes
    Modify run file for functional parameters.
    Modify json database access file

--- 2/news_scraper/run.py
+++ 2/news_scraper/run.py
@@ -1,5 +1,6 @@
 from crawler.outline import Outline
-import env
+from controller.scrapers import scrape
+from test.aosfatos import aos_fatos_test
 import json
 import nltk
 import os
@@ -18,21 +19,22 @@ def run(argv):
     """
     Execute and generate json file.
     """
-    interface = False
-
+    # Verify inicialization argument
     if "-init" in argv:
         initialize()
 
-    # Verify interface use:
-    if '-I' in argv:
-        interface = True
+    # Verify interface use for single news scrape:
+    if '--interface' in argv or '-I' in argv:
+        url_news = argv[-1]
+        outline = Outline(interface=True)
+        data = outline.getNews(url_news)
+        file_path = os.environ.get('COLLECTED_DIR') + json.loads(data)['title'] + ".json"
+        with open(file_path, 'w', encoding='utf8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False)
 
-    url_news = argv[-1]
-    outline = Outline(interface)
-    data = outline.getNews(url_news)
-    file_path = os.environ.get('COLLECTED_DIR') + json.loads(data)['title'] + ".json"
-    with open(file_path, 'w', encoding='utf8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False)
+    # Verify for routine execution
+    if '--routine' in argv or '-r' in argv:
+        scrape()
 
 
 if __name__ == "__main__":
