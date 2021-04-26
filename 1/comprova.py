acc3afc63dcc8d473ab82e5bf7d60c167b347ef1
samuelassis <samuelassisvieira@gmail.com>
2021-04-26 10:39:07 -0300

    comprova.py done

--- 1/comprova.py
+++ 1/comprova.py
@@ -9,9 +9,11 @@ from selenium.common.exceptions import NoSuchElementException
 
 class Comprova():
 	def __init__(self):
-		#self.text_locator = 
+		self.text_locator = (By.CSS_SELECTOR,'.answer__content--main')
+		self.header_text_locator = (By.CLASS_NAME,'answer__tag__details')
 		self.title_locator =(By.CLASS_NAME,'answer__title')
 		self.image_locator = (By.TAG_NAME,'img')
+		self.button_locator = (By.CLASS_NAME, 'answer__content__expand__label')
 		#self.author_locator =  não tem
 		self.category_locator =(By.CLASS_NAME,'answer__term')
 		self.verdict_locator = (By.CLASS_NAME,'answer__tag')
@@ -24,24 +26,28 @@ class Comprova():
 				'Imagem':'',
 				'Veredito':'',
 				'Html':''}
+
 		driver.get(url.strip())
+
 		wrapper = driver.find_element(*self.main_wrapper_locator)
-		category = wrapper.find_element(*self.category_locator).text
-		title = wrapper.find_element(*self.title_locator).text
-		image = wrapper.find_element(*self.image_locator).get_attribute('src')
-		verdict = wrapper.find_element(*self.verdict_locator).text
-		#text = 
-		infos['Titulo'] = title
-		#infos["Texto"]  = text  
-		infos['Categoria'] = category
-		infos['Imagem'] = image
-		infos['Veredito'] = verdict
+		button = WebDriverWait(driver, 30).until(EC.presence_of_element_located(self.button_locator))
+		driver.execute_script("arguments[0].click();", button)
+		time.sleep(1)
+		main_txt = wrapper.find_element(*self.text_locator).text
+	
+
+		infos['Titulo'] = wrapper.find_element(*self.title_locator).text
+		infos["Texto"]  = wrapper.find_element(*self.header_text_locator).text +'\n'+ main_txt
+		infos['Categoria'] = wrapper.find_element(*self.category_locator).text
+		infos['Imagem'] = wrapper.find_element(*self.image_locator).get_attribute('src')
+		infos['Veredito'] = wrapper.find_element(*self.verdict_locator).text
 		#infos['Html'] = driver.page_source
 		driver.close()
 		return infos
 
 C = Comprova()
 u = 'https://projetocomprova.com.br/publica%C3%A7%C3%B5es/video-deturpa-informacoes-sobre-a-atuacao-do-governo-na-preservacao-da-amazonia/'
+#u2 = 'https://projetocomprova.com.br/publica%C3%A7%C3%B5es/media-de-mortes-em-2020-nao-foi-menor-que-em-2019/'
 d = C.crawl_info(u)
 print(d)
 
