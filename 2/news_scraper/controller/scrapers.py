f42f0ce644b4bca262df74e01e90a886120e52d2
BrenoCPimenta <brenocpimenta@gmail.com>
2021-05-14 17:11:27 -0300

    Add json logic for historic_scraper()

--- 2/news_scraper/controller/scrapers.py
+++ 2/news_scraper/controller/scrapers.py
@@ -1,53 +1,71 @@
-from crawler.custom_crawler import CustomCrawler
+from crawler.custom.lupa_scraper import LupaScraper
 from crawler.outline import Outline
-from mapper.json_db_mapper import get_database_json_format
-from mapper.url_mapper import get_source_information
-from repository.json_db import insert_multiple_news
+from repository.json_db import insert
 from repository.json_db import select_urls
 import time
+import statistics
 
 
-def scrape():
+def routine_scraper():
     """
     Controller responsible for executing routine of data collector.
     """
+    pass
+
+
+def historic_scraper(source, save_rate):
+    """
+    Controller responsible for executing data collector on historic URLs.
+    """
     # Get urls
-    urls = select_urls()['urls']
-    # Initialize dictionaries to store results
-    formatted_news = {}
-    formatted_checking = {}
-    # Initialize scrapers objects
-    outline = Outline()
-    custom_crawler = CustomCrawler()
+    urls = select_urls(source)['urls']
+    # Initialize variables
+    time_success = []
+    time_failure = []
+    scraped_data = {}
+    scraped_htmls = {}
+    errors = {}
+    current_month = 0
+    # Initialize scraper instance
+    if source == 'lupa':
+        scraper = LupaScraper()
+    else:
+        scraper = Outline()
     # Execute
     initial_time = time.time()
-    for url in urls:
+    for idx, url in enumerate(urls):
         partial_time = time.time()
-        source, information = get_source_information(url)
-        # Select type of search by url source
-        if information['has_custom_crawler']:
-            data = custom_crawler.get_news(
-                url_news=url,
-                elements=information['elements'],
-                wait_page=information['has_js'])
-        else:
-            data = outline.get_news(url)
-        json_formatted = get_database_json_format(source, data)
-        if information['type'] == 'fact_checking':
-            formatted_checking.update(json_formatted)
-        elif information['type'] == 'news':
-            formatted_news.update(json_formatted)
-        else:
-            raise 'Unspecified url type for database insertion of collected data'
-        this_time = int(round(abs(partial_time - time.time()), 0))
-        print(source, " url scraped in ", this_time, " seconds")
-
-    outline.close_connection()
-    custom_crawler.close_connection()
+        try:
+            # Execute scraper
+            year, month, data, html = scraper.execute(url)
+            time_success.append(int(round(abs(partial_time - time.time()), 0)))
+            # Every 'save_rate' executions or if the month changes, it saves the collected data
+            if idx % save_rate == 0 or current_month != month:
+                current_month = month
+                # Insert collected data into database
+                insert(
+                    type='checking',
+                    source=source,
+                    collected_data=scraped_data,
+                    year=year,
+                    month=month,
+                    url_htmls=scraped_htmls
+                    )
+                insert(type='error', source=source, collected_data=errors)
+                # Clears the dictionaries for new scraped data
+                scraped_data = {}
+                scraped_htmls = {}
+                errors = {}
+            scraped_data.update(data)
+            scraped_htmls.update({url: html})
+        except Exception as e:
+            # If a exception is raised during the research, the Exception
+            # is saved with the url as keY.
+            errors.update({url: str(e)})
+            time_failure.append(int(round(abs(partial_time - time.time()), 0)))
+    scraper.close_connection()
     full_time = int(round(abs(initial_time - time.time()), 0))
     print(len(urls), " url's scraped in ", full_time, "seconds")
-    print('Saving data on database...')
-    # Insert collected data into database
-    insert_multiple_news(type='news', collected_data=formatted_news)
-    insert_multiple_news(type='checking', collected_data=formatted_checking)
+    print("Success mean time: ", statistics.mean(time_success))
+    print("Failure mean time: ", statistics.mean(time_failure))
     print('Done')
