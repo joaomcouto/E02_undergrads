88185763554c39e0b3925ea975682c6da820495b
samuelassis <samuelassisvieira@gmail.com>
2021-06-15 18:07:46 -0300

    aliadosbrasil e conexaoamazonas

--- 3/REVISTAAMAZONIA_scrapper.py
+++ 3/REVISTAAMAZONIA_scrapper.py
@@ -1,25 +1,24 @@
 from news_crawler.base_scrapper import BaseScrapper
 from selenium.webdriver.common.by import By
 
-class JORNAL21BRASILScrapper(BaseScrapper):
+class REVISTAAMAZONIAScrapper(BaseScrapper):
     def __init__(self,interface):
         if interface:
-            super(JORNAL21BRASILScrapper, self).__init__("firefox")
+            super(REVISTAAMAZONIAScrapper, self).__init__("firefox")
         else:
-            super(JORNAL21BRASILScrapper, self).__init__()
+            super(REVISTAAMAZONIAScrapper, self).__init__()
 
-    main_wrapper_locator = (By.CLASS_NAME,'item-post-inner')
-    text_locator = (By.CLASS_NAME, 'post-body')
-    text_locator_internal = (By.TAG_NAME,'span')
-    textUndesirables = ["Anúncio"]
+    main_wrapper_locator = (By.CLASS_NAME,'td-ss-main-content')
+    text_locator = (By.CLASS_NAME, 'td-post-content')
+    text_locator_internal = (By.TAG_NAME,'p')
+    textUndesirables = []
 
-    scrapperSource = "JORNAL21BRASIL"
-    title_locator = (By.CLASS_NAME,'entry-header')
+    scrapperSource = 'REVISTAAMAZONIA'
+    title_locator = (By.CLASS_NAME,'td-post-title')
     title_locator_internal = (By.CLASS_NAME,'entry-title')
-    
-    date_locator = (By.CLASS_NAME,'entry-time')
-    #date_locator_internal = (By.XPATH, '//a[not(contains(text(),"Atualizado"))]')
-    date_locator_internal = (By.CLASS_NAME,'published')
+
+    date_locator = (By.CLASS_NAME,'td-post-date')
+    date_locator_internal = (By.TAG_NAME,'time')
 
     dateHasTime = True
 
@@ -34,9 +33,9 @@ class JORNAL21BRASILScrapper(BaseScrapper):
 
     subtitle_locator = 'NULL'
 
-    image_locator = (By.CLASS_NAME,'separator')
-    image_locator_internal = (By.TAG_NAME,'img')
-    image_locator_attribute = 'src'
+    image_locator = (By.CLASS_NAME,'td-post-featured-image')
+    image_locator_internal = (By.CLASS_NAME,'td-modal-image')
+    image_locator_attribute = 'href'
 
     #author_locator =  (By.XPATH ,'//span[@style="color: rgb(159, 159, 223);"]' )
     author_locator = 'NULL'
@@ -63,7 +62,7 @@ class JORNAL21BRASILScrapper(BaseScrapper):
     
 
 
-f = JORNAL21BRASILScrapper(1)
-data = f.scrap_article('https://www.jornal21brasil.com.br/2020/03/israel-sai-na-frente-e-ja-tem-vacina.html')
+f = REVISTAAMAZONIAScrapper(1)
+data = f.scrap_article('https://revistaamazonia.com.br/alemanha-envia-cobranca-de-130-bilhoes-de-libras-a-china-por-danos-causados-pelo-coronavirus/?fbclid=IwAR1wmPKZg7pn7Gx1lckrwLC56cgQmatCeq2dgLpTDwVrDkVpPGY56HcJqUM')
 f.append_article_to_txt(data)
 f.driver.quit()
---
+++ 3/RSAGORA_scrapper.py
@@ -0,0 +1,69 @@
+from news_crawler.base_scrapper import BaseScrapper
+from selenium.webdriver.common.by import By
+
+class RSAGORAScrapper(BaseScrapper):
+    def __init__(self,interface):
+        if interface:
+            super(RSAGORAScrapper, self).__init__("firefox")
+        else:
+            super(RSAGORAScrapper, self).__init__()
+
+    main_wrapper_locator = (By.TAG_NAME,'article')
+    text_locator = (By.ID,'mvp-content-main')
+    text_locator_internal = (By.TAG_NAME,'p')
+    textUndesirables = []
+
+    scrapperSource = "RSAGORA"
+    title_locator = (By.CLASS_NAME,'mvp-post-title')
+    title_locator_internal = 'NULL'
+
+    subtitle_locator = 'NULL'
+    
+    date_locator = (By.CLASS_NAME,'mvp-author-info-text')
+    #date_locator_internal = (By.XPATH, '//a[not(contains(text(),"Atualizado"))]')
+    date_locator_internal = (By.TAG_NAME, 'time')
+
+    dateHasTime = False
+
+    dateHasDateTimeAttribute = True
+    dateTimeAttribute = 'datetime'
+
+    dateEndingSeparator = "NULL"
+    dateTimeSeparator = " às "
+    hourMinuteSeparator = ":"
+    dayMonthYearSeparator = " de "
+    monthNeedsMapper = False
+
+
+
+    image_locator = (By.CLASS_NAME,'wp-caption')
+    image_locator_internal = (By.TAG_NAME,'img')
+    image_locator_attribute = 'src'
+
+    #author_locator =  (By.XPATH ,'//span[@style="color: rgb(159, 159, 223);"]' )
+    author_locator = (By.CLASS_NAME,'author-name')
+    author_locator_internal = (By.TAG_NAME,'a')
+    author_locator_attribute = 'NULL'
+
+    category_locator = (By.CLASS_NAME, 'mvp-post-cat') #TEMPORARIO PQ TEM TAGS TEM QUE IMPLEMENTAR ISSO
+    category_locator_internal = 'NULL'
+
+
+    video_locator = "NULL"
+    
+    undesirables = []
+    
+    addUrlCategories = False
+    urlCategoryLowerBounds = []
+    urlCategoryUpperBounds = []
+    """
+    MANUAL CATEGORIES (MUDAR DE ACORDO COM O EDITORAL SOBRE O QUAL O CRAWLING DE URLS FOR FEITO)
+                Necessario pois no inicio da pandemia por exemplo no g1, noticias de covid tem apenas a categoria "ciencia-e-saude"
+                Alem disso, outro exemplo, noticias de politica relacionadas a covid frequentente recebem apenas a categoria "politica"
+    """
+    manualCategories = []
+
+d = RSAGORAScrapper(1)
+data = d.scrap_article("https://www.rsagora.com.br/detento-solto-por-medo-do-coronavirus-e-preso-com-grande-quantidade-de-drogas-e-armas/?fbclid=IwAR2mKVYla2Zb_arsluMtY-QqZpdMhSHd3AQtXhiRaq1O_ZlOVtG0m2la4ik")
+d.append_article_to_txt(data)
+d.driver.quit()
