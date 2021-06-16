45cba694db9e07eb581ffa61296692f8bd9e07eb
BrenoCPimenta <brenocpimenta@gmail.com>
2021-06-16 16:18:04 -0300

    Modify CARTAPIAUI to match template

--- 3/CARTAPIAUI_scrapper.py
+++ 3/CARTAPIAUI_scrapper.py
@@ -1,6 +1,7 @@
 from news_crawler.base_scrapper import BaseScrapper
 from selenium.webdriver.common.by import By
 
+
 class CARTAPIAUIScrapper(BaseScrapper):
     def __init__(self,interface):
         if interface:
@@ -8,26 +9,23 @@ class CARTAPIAUIScrapper(BaseScrapper):
         else:
             super(CARTAPIAUIScrapper, self).__init__()
 
+    scrapperSource = "CARTAPIAUI"
+
     main_wrapper_locator = (By.CLASS_NAME, 'content-inner')
+
+    title_locator = (By.CLASS_NAME,'post-header')
+    title_locator_internal = (By.CLASS_NAME, 'post-title')
+
     text_locator = (By.CLASS_NAME,'post-body-inner')
     text_locator_internal = (By.TAG_NAME,'p')
     textUndesirables = []
 
-    scrapperSource = "CARTAPIAUI"
-    title_locator = (By.CLASS_NAME,'post-header')
-    title_locator_internal = (By.CLASS_NAME, 'post-title')
-
-    subtitle_locator = 'NULL'
-    
     date_locator = (By.CLASS_NAME,'post-meta-wrapper')
-    #date_locator_internal = (By.XPATH, '//a[not(contains(text(),"Atualizado"))]')
     date_locator_internal = 'NULL'
-
     dateHasTime = True
-
     dateHasDateTimeAttribute = False
     dateTimeAttribute = 'NULL'
-
+    dateStartSeparator = "NULL"
     dateEndingSeparator = "NULL"
     dateTimeSeparator = " às "
     hourMinuteSeparator = ":"
@@ -47,27 +45,26 @@ class CARTAPIAUIScrapper(BaseScrapper):
         'novembro': 11,
         'dezembro': 12
     }
+    yearNeedsMapper = False
 
+    category_locator = 'NULL'
+    addUrlCategories = False
+    addTagsCategories = False
+    tagsUndesirables = []
+
+    subtitle_locator = 'NULL'
 
     image_locator = 'NULL'
     image_locator_internal = (By.TAG_NAME,'img')
     image_locator_attribute = 'src'
 
-    #author_locator =  (By.XPATH ,'//span[@style="color: rgb(159, 159, 223);"]' )
     author_locator = 'NULL'
     author_locator_attribute = 'NULL'
 
-    category_locator = 'NULL' #TEMPORARIO PQ TEM TAGS TEM QUE IMPLEMENTAR ISSO
-    category_locator_internal = 'NULL'
-
-
     video_locator = "NULL"
-    
+
     undesirables = []
-    
-    addUrlCategories = False
-    urlCategoryLowerBounds = []
-    urlCategoryUpperBounds = []
+
     """
     MANUAL CATEGORIES (MUDAR DE ACORDO COM O EDITORAL SOBRE O QUAL O CRAWLING DE URLS FOR FEITO)
                 Necessario pois no inicio da pandemia por exemplo no g1, noticias de covid tem apenas a categoria "ciencia-e-saude"
