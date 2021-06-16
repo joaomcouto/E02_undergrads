5487ad7cd9d97e44dc84748612030e8956675219
joaomcouto <joaom.couto55@gmail.com>
2021-06-16 08:41:41 -0300

    Updated TEMPLATE_scrapper and folder structure to reflect all newly created mandatory and optional parameters

--- 3/TEMPLATE_scrapper.py
+++ 3/TEMPLATE_scrapper.py
@@ -1,5 +1,6 @@
 from news_crawler.base_scrapper import BaseScrapper
 from selenium.webdriver.common.by import By
+from datetime import datetime
 
 class TEMPLATEScrapper(BaseScrapper):
     def __init__(self,interface):
@@ -8,60 +9,77 @@ class TEMPLATEScrapper(BaseScrapper):
         else:
             super(TEMPLATEScrapper, self).__init__()
 
-    ####OBRIGATORIOS
-    text_locator = (By. ,'')
-    scrapperSource = "TEMPLATE"
-    title_locator = (By. ,'')
-    main_wrapper_locator = (By. ,'')
-    date_locator = (By. , )
+    #Por default deixei todos os locators como By.X apenas porque X provou-se o mais comum para aquele parametro
+    #Mesma lógica se aplica aos booleanos e parametros attribute
+    #Para facilitar o copy&paste:
+        #(By.TAG_NAME,'')
+        #(By.ID,'')
+        #(By.XPATH,'')
 
-    ###PODEM SER NULL MAS PRECISAM SER DEFINIDOS
-    title_locator_internal = (By. '')
-    subtitle_locator = (By. ,'')
-    image_locator = (By. ,'')
-    author_locator =  (By. ,'')
-    author_locator_attribute = (By. ,'')
-    category_locator = (By. ,'')
-    video_locator = (By. ,'')
+    scrapperSource = "PORTALDENOTICIA" #OBRIGATORIO NÃO NULL
+
+    main_wrapper_locator = (By.CLASS_NAME,'main') #OBRIGATORIO NÃO NULL
+
+    title_locator = (By.CLASS_NAME,'') #OBRIGATORIO NÃO NULL
+    title_locator_internal = "NULL" #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+
+    text_locator = (By.CLASS_NAME,'')  #OBRIGATORIO NÃO NULL
+    text_locator_internal = "NULL" #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    textUndesirables = [] #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+
+    date_locator = (By.CLASS_NAME,'' ) #OBRIGATORIO NÃO NULL
+    date_locator_internal = "NULL" #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    dateHasTime = True #OBRIGATORIO NÃO NULL
+    dateHasDateTimeAttribute = False #OBRIGATORIO NÃO NULL
+    dateTimeAttribute = "datetime" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR FALSE
+    dateStartSeparator = "NULL" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    dateEndingSeparator = "NULL" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    dateTimeSeparator = "NULL" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    hourMinuteSeparator = "NULL" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    dayMonthYearSeparator = "NULL" #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    monthNeedsMapper = False #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    dateMonthMapper = {} #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    yearNeedsMapper = False #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+    dateYearMapper = {} #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR TRUE
+
+    ####Parametros abaixo desta linha são referentes a métodos que podem retornar "NULL" no caso de de erro. Acima desta linha, erros em métodos resultam em excessões que devem ser tratadas apenas para logging.
+    ### Isso significa que uma coleta é considerada sucesso mesmo que os campos categories, subtitles, image_url, video_url sejam "NULL" mesmo que por motivo de excessão.
+
+    category_locator = (By.CLASS_NAME, '') #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    category_locator_internal = 'NULL' #PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+    addUrlCategories = False #OBRIGATORIO NÃO NULL
+    urlCategoryLowerBounds = [''] #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR FALSE
+    urlCategoryUpperBounds = [''] #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR FALSE
+    addTagsCategories = True #OBRIGATORIO NÃO NULL
+    tags_categories_locator = (By.CLASS_NAME, '') #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR FALSE
+    tags_categories_locator_internal = (By.TAG_NAME,'') #PODE SER APAGADO SE O BOOL OBRIGATORIO ACIMA MAIS PROXIMO FOR FALSE
+    tagsUndesirables = [] #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+
+    subtitle_locator = "NULL" #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+
+    image_locator = (By.CLASS_NAME, '')#OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    image_locator_internal = (By.TAG_NAME,'') #PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+    image_locator_attribute = ''#PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+
+    author_locator =  (By.CLASS_NAME, '') #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    author_locator_internal = 'NULL' #PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+    author_locator_attribute = 'NULL'#PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+
+    video_locator = (By.CLASS_NAME, '') #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
+    video_locator_internal = 'NULL' #PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
+    video_locator_attribute = 'NULL'#PODE SER APAGADO SE O OBRIGATORIO ACIMA MAIS PROXIMO FOR "NULL"
     
-    ###SUBSTRINGS DE URLS INDESEJADAS
-    undesirables = ['']
+    undesirables = [] #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
     
-    #PARAMETROS PARA O METODO baseScrapper.get_date()
-    dateHasTime = bool
-
-    dateHasDateTimeAttribute = bool
-    dateTimeAttribute = 'datetime'
-
-    dateEndingSeparator = "NULL"
-    dateTimeSeparator = "NULL"
-    hourMinuteSeparator = "NULL"
-    dayMonthYearSeparator = "NULL"
-    monthNeedsMapper = "NULL"
-
-    #PARAMETROS PARA O METODO baseScrapper.get_category()
-    addUrlCategories = True
-    urlCategoryLowerBounds = [".br/"]
-    urlCategoryUpperBounds = ["/2020/","/2021/"]
     """
     MANUAL CATEGORIES (MUDAR DE ACORDO COM O EDITORAL SOBRE O QUAL O CRAWLING DE URLS FOR FEITO)
                 Necessario pois no inicio da pandemia por exemplo no g1, noticias de covid tem apenas a categoria "ciencia-e-saude"
                 Alem disso, outro exemplo, noticias de politica relacionadas a covid frequentente recebem apenas a categoria "politica"
     """
-    manualCategories = ['','']
+    manualCategories = [] #OBRIGATORIO MAS PODE SER "NULL"/VAZIO
 
-    #PARAMETROS ADICIONAIS PARA O METODO baseScrapper.get_main_image_url()
-    image_locator_internal = (By. , )
-    image_locator_attribute = ''
-
-    #PARAMETROS ADICIONAIS PARA O METODO baseScrapper.get_main_video_url()
-    video_locator_internal = "NULL"
-    video_locator_attribute = 'src'
-
-    #PARAMETROS get_text()
-    text_locator_internal = "NULL"
 
 t = TEMPLATEScrapper(0)
-data = t.scrap_article("https://")
+data = t.scrap_article("http://")
 t.append_article_to_txt(data)
 t.driver.quit()
