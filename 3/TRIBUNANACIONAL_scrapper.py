e0fe516c821a6ed988e8df2d5aa2ab816cc5cb35
BrenoCPimenta <brenocpimenta@gmail.com>
2021-06-18 02:20:29 -0300

    Modify TRIBUNANACIONAL to match template

--- 3/TRIBUNANACIONAL_scrapper.py
+++ 3/TRIBUNANACIONAL_scrapper.py
@@ -9,60 +9,53 @@ class TRIBUNANACIONALScrapper(BaseScrapper):
         else:
             super(TRIBUNANACIONALScrapper, self).__init__()
 
+    scrapperSource = "TRIBUNANACIONAL"
+
     main_wrapper_locator = (By.CLASS_NAME ,'pageNews' )
+
+    title_locator = (By.TAG_NAME ,'h1' )
+    title_locator_internal = "NULL"
 
     text_locator = (By.ID,'texto_release_format')
     text_locator_internal = "NULL"
     textUndesirables = []
 
-    scrapperSource = "TRIBUNANACIONAL"
-    title_locator = (By.TAG_NAME ,'h1' )
-    title_locator_internal = "NULL"
-    
     date_locator = (By.TAG_NAME ,'small' )
     date_locator_internal = "NULL"
-
     dateHasTime = True
-
     dateHasDateTimeAttribute = False
     dateTimeAttribute = "NULL"
-
+    dateStartSeparator = "NULL"
     dateEndingSeparator = "min - "
     dateTimeSeparator = " às "
     hourMinuteSeparator = "h"
     dayMonthYearSeparator = "/"
     monthNeedsMapper = False
-    dateMonthMapper = {}
+    yearNeedsMapper = False
+
+    category_locator = (By.XPATH ,'/html/body/section/div/div/div/h2')  #Vou ter que fazer funcao custom pq ta fora do main wrapper
+    category_locator_internal = 'NULL'
+    addUrlCategories = False
+    urlCategoryLowerBounds = []
+    urlCategoryUpperBounds = []
+    addTagsCategories = False
+    tags_categories_locator = 'NULL'
+    tags_categories_locator_internal = 'NULL'
+    tagsUndesirables = []
 
     subtitle_locator = "NULL"
 
     image_locator = (By.CLASS_NAME ,'boxhtml_full' ) #Não funciona e não tenho a menor ideia do porque
-    image_locator_internal = (By.TAG_NAME, "img") 
+    image_locator_internal = (By.TAG_NAME, "img")
     image_locator_attribute = 'src'
 
     author_locator =  (By.CLASS_NAME ,'fonte')
     author_locator_internal = "NULL"
     author_locator_attribute = 'NULL'
 
-    category_locator = (By.XPATH ,'/html/body/section/div/div/div/h2')  #Vou ter que fazer funcao custom pq ta fora do main wrapper
-    category_locator_internal = 'NULL'
-    
-    addUrlCategories = False
-    urlCategoryLowerBounds = []
-    urlCategoryUpperBounds = []
-
-    addTagsCategories = False
-    tags_categories_locator = 'NULL'
-    tags_categories_locator_internal = 'NULL'
-    tagsUndesirables = []
-
-
     video_locator = 'NULL'
-    video_locator_internal = 'NULL'
-    video_locator_attribute = 'NULL'
-    
+
     undesirables = []
-    
 
     """
     MANUAL CATEGORIES (MUDAR DE ACORDO COM O EDITORAL SOBRE O QUAL O CRAWLING DE URLS FOR FEITO)
@@ -89,5 +82,6 @@ class TRIBUNANACIONALScrapper(BaseScrapper):
         return list(set(categories))
 
 t = TRIBUNANACIONALScrapper(0)
-data = t.scrap_article("https://tribunanacional.com.br/coluna/151/passeando-pelas-redes-sociais--jair-bolsonaro-1990")
+#data = t.scrap_article("https://tribunanacional.com.br/coluna/151/passeando-pelas-redes-sociais--jair-bolsonaro-1990")
+data = t.scrap_article("https://tribunanacional.com.br/noticia/1252/cientistas-pedem-desculpas-ao-mundo--por-estudo-ter-desqualificado-o-uso-da-cloroquina-no-combate-a-pandemia-e-so-pedir-desculpas-e-esta-tudo-certo")
 t.driver.quit()
