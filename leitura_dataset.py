0c7c7f5d9bb96b7474034e9a64c55d3f304a306f
BrenoCPimenta <brenocpimenta@gmail.com>
2021-06-25 08:29:30 -0300

    Add leitura_dataset

---
+++ leitura_dataset.py
@@ -0,0 +1,25 @@
+import json
+import pprint
+
+
+def lendo_dataset(localizacao_arquivo):
+    # Leitura de dados:
+    dados = []
+    with open(localizacao_arquivo, mode='r') as f:
+        for line in f:
+            dados.append(json.loads(line.strip()))
+
+    # Imprimindo primeiro 3 coletas de cada
+    for i in range(3):
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(dados[i])
+        print('\n')
+
+
+# Lendo TrueNews
+print('\n---------------TRUE NEWS:\n')
+lendo_dataset('./DATASET_MPMG-TrueNews_selected.txt')
+
+print('\n\n\n---------------FAKE NEWS:\n')
+# Lendo FakeNews
+lendo_dataset('./DATASET_MPMG-FakeNews_matched.txt')
